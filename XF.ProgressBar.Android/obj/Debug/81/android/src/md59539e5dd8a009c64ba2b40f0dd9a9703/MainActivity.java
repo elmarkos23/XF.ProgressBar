ab7a2e0887614e40e9039cd8c318cc34a31b47db
Markos Ayala <elmarkos23@gmail.com>
2019-02-25 10:17:54 -0500

    Initial commit

---
+++ XF.ProgressBar.Android/obj/Debug/81/android/src/md59539e5dd8a009c64ba2b40f0dd9a9703/MainActivity.java
@@ -0,0 +1,47 @@
+package md59539e5dd8a009c64ba2b40f0dd9a9703;
+
+
+public class MainActivity
+	extends md51558244f76c53b6aeda52c8a337f2c37.FormsAppCompatActivity
+	implements
+		mono.android.IGCUserPeer
+{
+/** @hide */
+	public static final String __md_methods;
+	static {
+		__md_methods = 
+			"n_onCreate:(Landroid/os/Bundle;)V:GetOnCreate_Landroid_os_Bundle_Handler\n" +
+			"";
+		mono.android.Runtime.register ("XF.ProgressBar.Droid.MainActivity, XF.ProgressBar.Android", MainActivity.class, __md_methods);
+	}
+
+
+	public MainActivity ()
+	{
+		super ();
+		if (getClass () == MainActivity.class)
+			mono.android.TypeManager.Activate ("XF.ProgressBar.Droid.MainActivity, XF.ProgressBar.Android", "", this, new java.lang.Object[] {  });
+	}
+
+
+	public void onCreate (android.os.Bundle p0)
+	{
+		n_onCreate (p0);
+	}
+
+	private native void n_onCreate (android.os.Bundle p0);
+
+	private java.util.ArrayList refList;
+	public void monodroidAddReference (java.lang.Object obj)
+	{
+		if (refList == null)
+			refList = new java.util.ArrayList ();
+		refList.add (obj);
+	}
+
+	public void monodroidClearReferences ()
+	{
+		if (refList != null)
+			refList.clear ();
+	}
+}
---
+++ XF.ProgressBar/MainPage.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace XF.ProgressBar
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+        private void funCargar()
+        {
+            pbCargarndo.Progress = 0;
+            Task.Run(() => {
+                for (int i = 0; i < 1000; i++)
+                {
+                    Thread.Sleep(5);
+                    pbCargarndo.Progress = Convert.ToDouble("00." + i.ToString());
+                    pbCargarndo.Progress = i;
+                }
+            });
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            funCargar();
+        }
+    }
+}
